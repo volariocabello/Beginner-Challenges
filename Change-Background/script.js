543ca6f83306268c534c5abe3c7f55a4145c9dc1
volariocabello <125208798+volariocabello@users.noreply.github.com>
2023-02-19 17:38:05 +0200

    Delete Change-Background directory

--- Change-Background/script.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("button");
-
-const changeColor = () => {
-    const hexVal = Math.floor(Math.random() * 0xffffff).toString(16);
-    document.body.style.background = `#${hexVal}`;
-}
-
-button.addEventListener("click", changeColor);
